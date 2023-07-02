6e974c0c2f0b0e7723de08964b044b66d520efda
klkuhlm <kristopher.kuhlman@gmail.com>
2023-07-02 16:31:23 -0600

    added different normalization, now shift of stormont data looks better

--- stormont-data/convert-raw-stormont-data.py
+++ stormont-data/convert-raw-stormont-data.py
@@ -21,6 +21,8 @@ bores = {}
 
 fig, ax = plt.subplots(1, 1, num=1, figsize=(6, 4))
 
+fig2, axes = plt.subplots(1, 2, num=2, figsize=(8, 4), constrained_layout=True)
+
 for fn in glob("??-*r.csv"):
     bore_no = fn.split("-")[0]
     bore_r = float(fn.split("-")[1][:-5])  # drop "r.csv" from end
@@ -35,24 +37,42 @@ for fn in glob("??-*r.csv"):
     print(d[bore_no][:5, :])
 
     delta = np.zeros_like(d[bore_no])
-    delta[:, 0] = d[bore_no][:, 0] - mineby_time # delta time
+    delta[:, 0] = d[bore_no][:, 0] - mineby_time  # delta time
 
     if 0:
-        idx = np.argmin(np.abs(delta[:, 0])) # value closest to zero time (pos or neg)
+        idx = np.argmin(np.abs(delta[:, 0]))  # value closest to zero time (pos or neg)
+        norm_t = d[bore_no][idx, 0]
+        norm_p = d[bore_no][idx, 1]
+    if 0:
+        idx = np.argmax(d[bore_no][:, 1])
+        norm_t = d[bore_no][idx, 0]
         norm_p = d[bore_no][idx, 1]
     if 1:
-        idx = np.argmax(d[bore_no][:,1])
-        norm_p = d[bore_no][idx,1]
+        avg_mask = np.logical_and(
+            d[bore_no][:, 0] >= 145 * DAY2SEC, d[bore_no][:, 0] <= mineby_time
+        )
+        norm_t = np.median(d[bore_no][avg_mask, 0])
+        norm_p = np.median(d[bore_no][avg_mask, 1])
 
-    print(norm_p,idx,delta[idx,0]/DAY2SEC)
-    delta[:, 1] = (norm_p - d[bore_no][:, 1])/norm_p # delta relative pressure
+    print(norm_p, norm_t / DAY2SEC)
+    delta[:, 1] = (norm_p - d[bore_no][:, 1]) / norm_p  # delta relative pressure
 
     sidx = np.argsort(delta[:, 0])
     sdelta = delta[sidx, :]
     pos_mask = sdelta[:, 0] > 0.0
 
+    # original data
+    axes[0].plot(
+        d[bore_no][sidx, 0] / DAY2SEC, d[bore_no][sidx, 1], lw=0.25, marker="."
+    )
+    axes[0].plot(mb[:, 0], mb[:, 1], "k--")
+
+    # drop in pressure (not normalized)
+    axes[1].plot(d[bore_no][sidx, 0] / DAY2SEC, delta[sidx, 1] * norm_p)
+    axes[1].plot(mb[:, 0], mb[:, 1], "k--")
+
     ax.loglog(
-        sdelta[pos_mask, 0]/DAY2SEC,
+        sdelta[pos_mask, 0] / DAY2SEC,
         sdelta[pos_mask, 1],
         label=f"{bore_no} {bore_r}r",
         lw=0.25,
@@ -61,10 +81,22 @@ for fn in glob("??-*r.csv"):
 
     np.savetxt(fn.replace(".csv", "-drawdown.csv"), sdelta[pos_mask], fmt="%.3e")
 
+axes[0].set_xlabel("days")
+axes[0].set_ylabel("pressure [MPa]")
+axes[0].grid(True)
+axes[0].set_ylim([mb[1, 1], mb[0, 1]])
+axes[1].set_xlabel("days")
+axes[1].set_ylabel("$\\Delta$ pressure [MPa]")
+axes[1].grid(True)
+axes[1].set_ylim([-0.25, 3])
+
 ax.set_xlabel("time since mineby [day]")
 ax.set_ylabel("relative $\\Delta P$ since mineby [MPa]")
 ax.grid(True)
 ax.legend(loc=0, fontsize="small")
 
-fig.savefig("drawdown.png", dpi=200)
+fig2.savefig("pressure.png", dpi=200)
+plt.close(2)
+
+fig.savefig("scaled_drawdown.png", dpi=200)
 plt.close(1)
